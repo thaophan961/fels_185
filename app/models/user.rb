65d1a98882173d4ce7ed594cd1bd01dd14d41ec0
Phan Thi Thu Thao <phan.thi.thu.thao@framgia.com>
2016-09-28 15:38:19 +0700

    show index user

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,7 @@ class User < ActiveRecord::Base
     format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
   validates :password, presence: true, length: {minimum: 6}, allow_nil: true
   has_secure_password
+  scope :alphabet, ->{order :name}
 
   before_save :downcase_email
 
