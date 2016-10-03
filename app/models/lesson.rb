c2bf42643ece8bc29650de2c19c53ff327cff515
Do Thi Diem Thao <do.thi.diem.thao@framgia.com>
2016-10-03 15:55:43 +0700

    78864 Show activities of user

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -7,6 +7,15 @@ class Lesson < ActiveRecord::Base
   accepts_nested_attributes_for :results
   before_create :build_results
   delegate :title, to: :category, allow_nil: true
+  scope :recent, ->{order "created_at DESC"}
+  scope :feed_activities, ->user_id do
+    where "user_id IN (SELECT followed FROM relationships WHERE follower = ?)
+      OR user_id = ?", user_id, user_id
+  end
+
+  def score
+    answers.correct.size
+  end
 
   private
   def build_results
