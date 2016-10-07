bfe85d56e5a73240cf598f3031f104f866a180ec
Do Thi Diem Thao <do.thi.diem.thao@framgia.com>
2016-10-07 09:15:43 +0700

    Export words to csv excel

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,7 @@ class StaticPagesController < ApplicationController
     if logged_in?
       @activities = current_user.feed_activities.recent
         .paginate page: params[:page], per_page: Settings.per_page_activities
+      @words = Word.includes(:answers).learned current_user.id
     end
   end
 end
