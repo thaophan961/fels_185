5dc178e7ff46c39aa81197e71ef23a7013ea973b
Phan Thi Thu Thao <phan.thi.thu.thao@framgia.com>
2016-10-07 13:17:46 +0700

    admin create category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,6 +1,27 @@
-class Admin::CategoriesController < ApplicationController
+class Admin::CategoriesController < Admin::BaseController
+  before_action :logged_in_user, only: [:index, :new, :create]
+
   def index
     @categories = Category.recent.paginate page: params[:page],
       per_page: Settings.per_page
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new category_params
+    if @category.save
+      flash[:success] = t "category.message_success"
+      redirect_to admin_categories_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def category_params
+    params.require(:category).permit :title, :quantity_question
   end
 end
